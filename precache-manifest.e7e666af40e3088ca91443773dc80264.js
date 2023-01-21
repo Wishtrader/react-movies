08b78f20a515c0edd08c81887239c88734c80488
Andrei Kamarou <mr.akomarov@gmail.com>
2023-01-21 22:40:36 +0300

    Updates

---
+++ precache-manifest.e7e666af40e3088ca91443773dc80264.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "105b454f399073d8d758",
+    "url": "/react-movies/static/css/main.d9a6718d.chunk.css"
+  },
+  {
+    "revision": "105b454f399073d8d758",
+    "url": "/react-movies/static/js/main.105b454f.chunk.js"
+  },
+  {
+    "revision": "022280761d4275f8b4b5",
+    "url": "/react-movies/static/js/1.02228076.chunk.js"
+  },
+  {
+    "revision": "6e0964696bb83cd24dee",
+    "url": "/react-movies/static/js/runtime~main.6e096469.js"
+  },
+  {
+    "revision": "55e21c900938fa3d3cfa23a05bbeb4da",
+    "url": "/react-movies/index.html"
+  }
+];
